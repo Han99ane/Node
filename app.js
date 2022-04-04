306fd730438a6dae3bf89e9bfe0fd03d57e168da
Han99ane <102959678+Han99ane@users.noreply.github.com>
2022-04-04 12:23:27 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Blue environment");
+  res.send("First version");
 });
 
 app.listen(process.env.PORT || 5000);
